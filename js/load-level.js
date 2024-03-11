313f2cd389b4e409abbb6b73776905b3720e178f
Jed Staley <JNStaley1@gmail.com>
2024-03-11 11:13:48 -0400

    moved loadDoor function code into file load-level.js along with character and key load functions, then called them in app.js.

---
+++ js/load-level.js
@@ -0,0 +1,38 @@
+function loadChar () {
+  let char = document.createElement('img');
+  
+  char.id = 'mainChar'
+  char.src = 'mainChar.jpg';
+  char.alt = 'Main Character';
+
+  let mainElement = document.querySelector('main');
+  mainElement.appendChild(char);
+}
+
+function loadDoor () {
+  let door = document.createElement('img');
+  
+  door.id = 'door'
+  door.src = 'door.jpg';
+  door.alt = 'Door';
+
+  let mainElement = document.querySelector('main');
+  mainElement.appendChild(door);
+}
+
+function loadKey () {
+  let key = document.createElement('img');
+  
+  key.id = 'key'
+  key.src = 'key.jpg';
+  key.alt = 'Key';
+
+  let mainElement = document.querySelector('main');
+  mainElement.appendChild(key);
+}
+
+function loadLevel() {
+  loadChar();
+  loadDoor();
+  loadKey();
+}
