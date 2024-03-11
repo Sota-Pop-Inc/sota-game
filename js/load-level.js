ce7c0f00bd839c74f98fbb845d56ae1a6fa7a480
Jed Staley <JNStaley1@gmail.com>
2024-03-11 12:57:04 -0400

    added custom positions for images and corrected image file stricture and paths

--- js/load-level.js
+++ js/load-level.js
@@ -4,7 +4,7 @@ function loadChar () {
   let char = document.createElement('img');
 
   char.id = 'mainChar';
-  char.src = './character/mainChar.png';
+  char.src = 'assets/character/mainChar.png';
   char.alt = 'Main Character';
 
   let mainElement = document.getElementById('main');
@@ -18,28 +18,28 @@ function loadDoor () {
   let door = document.createElement('img');
 
   door.id = 'door';
-  door.src = 'door.jpg';
+  door.src = 'assets/props/door_locked.png';
   door.alt = 'Door';
 
   let mainElement = document.getElementById('main');
   mainElement.appendChild(door);
 
-  GameContents.door.coordinates.x = 0;
-  GameContents.door.coordinates.y = 0;
+  GameContents.door.coordinates.x = 1200;
+  GameContents.door.coordinates.y = 300;
 }
 
 function loadKey () {
   let key = document.createElement('img');
 
   key.id = 'key';
-  key.src = 'key.jpg';
+  key.src = 'assets/props/key.png';
   key.alt = 'Key';
 
   let mainElement = document.getElementById('main');
   mainElement.appendChild(key);
 
-  GameContents.key.coordinates.x = 0;
-  GameContents.key.coordinates.y = 0;
+  GameContents.key.coordinates.x = 200;
+  GameContents.key.coordinates.y = 500;
 }
 
 function loadLevel() {
