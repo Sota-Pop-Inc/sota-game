713bf92ef456411ef70b33c8106246d892ad20a7
Jed Staley <JNStaley1@gmail.com>
2024-03-11 14:58:33 -0400

    edited out unecessary console logs

--- js/collision.js
+++ js/collision.js
@@ -2,16 +2,12 @@
 
 function checkSpecCollision(object) {
   if (GameContents.mainChar.coordinates.x > GameContents[object].bottomRight.x) {
-    console.log('uh oh tripped 1');
     return false;
   } else if (GameContents.mainChar.bottomRight.x < GameContents[object].coordinates.x) {
-    console.log('uh oh tripped 2');
     return false;
   } else if (GameContents.mainChar.coordinates.y > GameContents[object].bottomRight.y) {
-    console.log('uh oh tripped 3');
     return false;
   } else if (GameContents.mainChar.bottomRight.y < GameContents[object].coordinates.y) {
-    console.log('uh oh tripped 4');
     return false;
   } else {
     return true;
@@ -19,11 +15,10 @@ function checkSpecCollision(object) {
 }
 
 function collectKey() {
-  console.log('checking for key');
   if (checkSpecCollision('key')) {
     GameContents.mainChar.inventory.push('key');
     console.log('we got da key yippee');
-  } else {console.log('no key');}
+  }
 }
 
 function useDoor() {
